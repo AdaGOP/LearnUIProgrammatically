33d4ed7f5a322a6b3d52fa442f33780443828a74
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2022-09-19 21:56:12 +0700

    finish refactoring UI using reusable component (UILabel, UIButton, UIStackView)

---
+++ LearnUIProgrammatically/Theme/ReusableStackView.swift
@@ -0,0 +1,79 @@
+//
+//  ReusableStackView.swift
+//  LearnUIProgrammatically
+//
+//  Created by khoirunnisa' rizky noor fatimah on 19/09/22.
+//
+
+import UIKit
+
+class ReusableStackView: UIStackView {
+    
+    enum axisEnum {
+        case horizontal, vertical
+    }
+    
+    enum distributionEnum {
+        case fillProportionally, fill
+    }
+    
+    var spacingValue: CGFloat
+    var subView: [UIView]
+    var axisValue: axisEnum? {
+        didSet {
+            testAxisValue()
+        }
+    }
+    var distributionValue: distributionEnum? {
+        didSet {
+            testDistributionValue()
+        }
+    }
+    
+    init(spacingValue: CGFloat, subView: [UIView], axisValue: axisEnum?, distributionValue: distributionEnum?) {
+        self.spacingValue = spacingValue
+        self.subView = subView
+        super.init(frame: .zero)
+        self.axisValue = axisValue
+        self.distributionValue = distributionValue
+        self.setup()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setup() {
+        self.spacing = spacingValue
+        for view in subView {
+            self.addArrangedSubview(view)
+        }
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.testAxisValue()
+        self.testDistributionValue()
+        
+    }
+    
+    private func testAxisValue() {
+        switch axisValue {
+        case .horizontal:
+            self.axis = .horizontal
+        case .vertical:
+            self.axis = .vertical
+        default:
+            print("none")
+        }
+    }
+    
+    private func testDistributionValue() {
+        switch distributionValue {
+        case .fillProportionally:
+            self.distribution = .fillProportionally
+        case .fill:
+            self.distribution = .fill
+        default:
+            print("none")
+        }
+    }
+
+}
--- LearnUIProgrammatically/ViewController.swift
+++ LearnUIProgrammatically/ViewController.swift
@@ -8,70 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private lazy var titleLabel: UILabel = {
-        let titleLbl = UILabel()
-        titleLbl.text = "Hello and welcome!"
-        titleLbl.font = .boldSystemFont(ofSize: 24)
-        titleLbl.translatesAutoresizingMaskIntoConstraints = false
-        return titleLbl
-    }()
     
-    private lazy var bodyLabel: UILabel = {
-        let bodyLbl = UILabel()
-        bodyLbl.text = "This place is not safe. You need two weapons below to help you attack your enemy."
-        bodyLbl.numberOfLines = 0
-        bodyLbl.translatesAutoresizingMaskIntoConstraints = false
-        return bodyLbl
-    }()
-    
+    private lazy var titleLabel = ReusableLabel(labelText: "Hello and welcome!", labelType: .title)
+    private lazy var bodyLabel = ReusableLabel(labelText: "This place is not safe. You need two weapons below to help you attack your enemy.", labelType: .subtitle)
+    private lazy var axeButton = ReusableButton(title: "Axe", color: .blue)
+    private lazy var gunButton = ReusableButton(title: "Gun", color: .red)
+    private lazy var buttonStackView = ReusableStackView(spacingValue: 5, subView: [gunButton, axeButton], axisValue: .horizontal, distributionValue: .fillProportionally)
     private lazy var spacer: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
         view.heightAnchor.constraint(equalToConstant: 10).isActive = true
         return view
     }()
-    
-    private lazy var axeButton: UIButton = {
-        let blueBtn = UIButton()
-        blueBtn.backgroundColor = .blue
-        blueBtn.setTitle("Axe", for: .normal)
-        blueBtn.tintColor = .white
-        blueBtn.layer.cornerRadius = 5
-        blueBtn.clipsToBounds = true
-        blueBtn.translatesAutoresizingMaskIntoConstraints = false
-        return blueBtn
-    }()
-    
-    private lazy var gunButton: UIButton = {
-        let redBtn = UIButton()
-        redBtn.backgroundColor = .red
-        redBtn.setTitle("Gun", for: .normal)
-        redBtn.tintColor = .white
-        redBtn.layer.cornerRadius = 5
-        redBtn.clipsToBounds = true
-        redBtn.translatesAutoresizingMaskIntoConstraints = false
-        return redBtn
-    }()
-    
-    private lazy var buttonStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [gunButton, axeButton])
-        stackView.axis = .horizontal
-        stackView.distribution = .fillProportionally
-        stackView.spacing = 5
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
-    private lazy var stackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [titleLabel, bodyLabel, spacer, buttonStackView])
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        stackView.spacing = 6
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        return stackView
-    }()
-    
+    private lazy var stackView = ReusableStackView(spacingValue: 5, subView: [titleLabel, bodyLabel, spacer, buttonStackView], axisValue: .vertical, distributionValue: .fill)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
